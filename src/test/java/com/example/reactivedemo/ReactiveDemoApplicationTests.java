350b85918e6a460f4ad3712fac3c7c1b165ffe36
moocstudent <343840681@qq.com>
2020-10-28 18:46:29 +0800

    ~junit5

--- src/test/java/com/example/reactivedemo/ReactiveDemoApplicationTests.java
+++ src/test/java/com/example/reactivedemo/ReactiveDemoApplicationTests.java
@@ -3,17 +3,23 @@ package com.example.reactivedemo;
 import org.junit.Before;
 import org.junit.gen5.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.condition.OS;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.DisabledIf;
 import org.springframework.test.context.junit.jupiter.EnabledIf;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 /*
 https://blog.csdn.net/io_field/article/details/82801067
+
 JUnit 5
+https://www.codetd.com/article/4927518
  */
 @SpringBootTest
 class ReactiveDemoApplicationTests {
@@ -22,8 +28,15 @@ class ReactiveDemoApplicationTests {
 
     @Test
     @EnabledOnJre(JRE.JAVA_8)
+    @EnabledOnOs({ OS.WINDOWS, OS.MAC })
     public void onlyOnJava8() {
         System.out.println("该测试在JDK 8中启用");
+        System.out.println("该测试在Windows/MAC系统中启用");
+    }
+
+    @RepeatedTest(10) // Dynamic JavaScript expression.
+    public void repeat10(){
+        System.out.println("这条随机测试会重复测试10次");
     }
 
     @Disabled
